f2f5d6df9a3dcc0a964587b2f4407b2e4c816a50
neko-neko <seiichi.nishikata456@gmail.com>
2018-06-18 12:37:38 +0900

    Update vscode setting

--- setup/install/12_vscode.zsh
+++ setup/install/12_vscode.zsh
@@ -7,10 +7,15 @@ extensions=(
   EditorConfig.EditorConfig
   PeterJausovec.vscode-docker
   dbaeumer.vscode-eslint
+  DotJoshJohnson.xml
   esbenp.prettier-vscode
   flowtype.flow-for-vscode
+  mauve.terraform
+  ms-python.python
   msjsdiag.debugger-for-chrome
+  naumovs.color-highlight
   nonylene.dark-molokai-theme
+  peterj.proto
   redhat.vscode-yaml
   Tyriar.sort-lines
   vsmobile.vscode-react-native
