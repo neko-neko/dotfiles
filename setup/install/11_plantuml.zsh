0b46bf26e92be2463eae89e1ddb46d21e2c220ac
neko-neko <seiichi.nishikata456@gmail.com>
2018-07-17 14:53:46 +0900

    Remove plantuml

--- setup/install/11_plantuml.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-source ${HOME}/.dotfiles/setup/util.zsh
-
-util::info 'install PlantUML...'
-
-mkdir -p ${HOME}/lib/java
-wget http://jaist.dl.sourceforge.net/project/plantuml/plantuml.jar -P ${HOME}/lib/java/
