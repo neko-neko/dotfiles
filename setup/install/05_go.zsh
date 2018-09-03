55c40b048d8b48f3b799c6ba9768696c27f492b4
neko-neko <seiichi.nishikata456@gmail.com>
2018-09-03 12:10:07 +0900

    Update gocode package

--- setup/install/05_go.zsh
+++ setup/install/05_go.zsh
@@ -5,7 +5,7 @@ util::info 'install golang and libs...'
 
 libs=(
   github.com/motemen/ghq
-  github.com/nsf/gocode
+  github.com/mdempsky/gocode
   github.com/davecheney/httpstat
   github.com/golang/protobuf/protoc-gen-go
   github.com/alecthomas/gometalinter
