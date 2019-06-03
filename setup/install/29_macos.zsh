d23e3e062cdd17678977f0e3e3127d308c06c0f4
neko-neko <seiichi.nishikata456@gmail.com>
2019-06-03 13:50:17 +0900

    Add macOS settings

--- setup/install/29_macos.zsh
+++ setup/install/29_macos.zsh
@@ -7,3 +7,11 @@ util::info 'update macOS settings...'
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
 defaults write com.apple.finder AppleShowAllFiles -boolean true
+
+# Dock
+defaults write com.apple.dock orientation left
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock persistent-apps -array
+
+# Finder
+defaults write -g AppleShowAllExtensions -bool true
