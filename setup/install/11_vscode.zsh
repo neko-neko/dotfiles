692d9175c410adb3485b4a73110b8226dea5cb03
neko-neko <seiichi.nishikata456@gmail.com>
2019-06-14 11:56:01 +0900

    Update settings of VSCode

--- setup/install/11_vscode.zsh
+++ setup/install/11_vscode.zsh
@@ -4,22 +4,28 @@ source ${HOME}/.dotfiles/setup/util.zsh
 util::info 'configure vscode...'
 
 extensions=(
+  bungcip.better-toml
   castwide.solargraph
+  CoenraadS.bracket-pair-colorizer-2
   dbaeumer.vscode-eslint
+  eamodio.gitlens
   EditorConfig.EditorConfig
   esbenp.prettier-vscode
+  GitHub.vscode-pull-request-github
   kumar-harsh.graphql-for-vscode
   mauve.terraform
   ms-kubernetes-tools.vscode-kubernetes-tools
   ms-python.python
+  ms-vscode-remote.remote-containers
   ms-vscode.Go
+  ms-vsliveshare.vsliveshare
   nonylene.dark-molokai-theme
   PeterJausovec.vscode-docker
   rebornix.ruby
   redhat.vscode-yaml
   shardulm94.trailing-spaces
+  VisualStudioExptTeam.vscodeintellicode
   vscode-icons-team.vscode-icons
-  vsmobile.vscode-react-native
   yzhang.markdown-all-in-one
   zxh404.vscode-proto3
 )
