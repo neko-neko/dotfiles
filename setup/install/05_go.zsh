9e4a06cfd5d3222f6f930e298da726e1672521af
neko-neko <seiichi.nishikata456@gmail.com>
2019-06-16 01:22:24 +0900

    Update go modules

--- setup/install/05_go.zsh
+++ setup/install/05_go.zsh
@@ -7,12 +7,10 @@ libs=(
   github.com/motemen/ghq
   github.com/golang/protobuf/protoc-gen-go
   github.com/alecthomas/gometalinter
-  github.com/saibing/bingo
+  go get -u golang.org/x/tools/cmd/gopls
 )
 
 brew install go
 for lib in ${libs[@]}; do
   go get -u -v ${lib}
 done
-
-gometalinter --install --update
